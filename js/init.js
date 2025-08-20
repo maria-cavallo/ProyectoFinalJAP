46a4449d711c078b70c89d5f43bd55857a9bac22
GuillermoLescano <guillermolescano28@gmail.com>
2025-08-20 15:03:53 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
 function getCookie(name){
-  const value=`; ${document.cookie}`
+  const value=`;${document.cookie}`
   const parts = value.split(`;${name}=`);
+  console.log(parts);
   if(parts.length===2) return decodeURIComponent(parts.pop().split(';').shift());
   return null;
 }
